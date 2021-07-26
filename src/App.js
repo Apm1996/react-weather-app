ad8fad63c572fdd794a259fc79a016aabae21bb5
Apm1996 <andreaperismagraner@gmail.com>
2021-07-26 19:00:39 +0200

    Added URL and footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by Andrea Peris with ♥️
+        <a href="#" target="_blank"></a>
+      </footer>
+    </div>
+  );
 }
