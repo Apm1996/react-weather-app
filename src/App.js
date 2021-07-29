2defc112b2e39381237b0091126c9364ff00f38d
Apm1996 <andreaperismagraner@gmail.com>
2021-07-29 20:03:21 +0200

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is coded by Andrea Peris with ♥️
-        <a href="#" target="_blank"></a>
-      </footer>
+      <div class="container">
+        <Weather />
+        <footer>
+          This project is coded by Andrea Peris with ♥️
+          <a href="#" target="_blank"></a>
+        </footer>
+      </div>
     </div>
   );
 }
